2ca67eec2238d6347048a524c27859ec15cd7d1c
tj <tjlangan17@gmail.com>
2024-08-30 11:01:52 -0400

    Identity matrix, eye()

--- src/linalg/matrix.zig
+++ src/linalg/matrix.zig
@@ -3,6 +3,7 @@ const Allocator = std.mem.Allocator;
 
 const Error = @import("errors.zig").LinalgError;
 const shape2cap = @import("shape2cap.zig").shape2cap;
+const sub2ind = @import("sub2ind.zig").sub2ind;
 const fill = @import("fill.zig").fill;
 
 pub fn Matrix(comptime T: type) type {
@@ -65,6 +66,18 @@ pub fn Matrix(comptime T: type) type {
             const matrix = try init(allocator, shape);
 
             fill(T, matrix.data, 1);
+
+            return matrix;
+        }
+
+        pub fn eye(allocator: Allocator, n: usize) Error!Self {
+            const matrix = try zeros(allocator, &.{ n, n });
+
+            for (0..n) |ii| {
+                const ind = try sub2ind(matrix.shape, &.{ ii, ii });
+
+                matrix.data[ind] = 1;
+            }
 
             return matrix;
         }
@@ -136,4 +149,26 @@ test "ones" {
     for (mat.data) |elem| {
         try std.testing.expectEqual(1, elem);
     }
+}
+
+test "eye" {
+    const allocator = std.testing.allocator;
+
+    const mat = try Matrix(u32).eye(allocator, 3);
+    defer mat.deinit(allocator);
+
+    try std.testing.expectEqual(3, mat.shape[0]);
+    try std.testing.expectEqual(3, mat.shape[1]);
+
+    try std.testing.expectEqual(1, mat.data[0]);
+    try std.testing.expectEqual(0, mat.data[1]);
+    try std.testing.expectEqual(0, mat.data[2]);
+
+    try std.testing.expectEqual(0, mat.data[3]);
+    try std.testing.expectEqual(1, mat.data[4]);
+    try std.testing.expectEqual(0, mat.data[5]);
+
+    try std.testing.expectEqual(0, mat.data[6]);
+    try std.testing.expectEqual(0, mat.data[7]);
+    try std.testing.expectEqual(1, mat.data[8]);
 }
