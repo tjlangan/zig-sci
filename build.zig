1c406e4218271f0d42fd2e520665af3be4235626
tj <tjlangan17@gmail.com>
2024-09-07 22:21:42 -0400

    converted from static lib to a module, also added some future ideas to implement

--- build.zig
+++ build.zig
@@ -4,14 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-sci",
+    _ = b.addModule("zig-sci", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
